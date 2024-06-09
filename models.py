197760c058091d9a4c5abee156848231cf5f7183
OuisMusic <60331398+OuisMusic@users.noreply.github.com>
2024-06-09 09:24:35 +0200

    cleaning

--- models.py
+++ models.py
@@ -4,7 +4,10 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///smarthome.db')
+database_uri = os.getenv('DATABASE_URL', 'sqlite:///smarthome.db')
+if database_uri.startswith("postgres://"):
+    database_uri = database_uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
