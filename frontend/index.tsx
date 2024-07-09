e144e5a2bd41ccb4fd7f300b37bd4ff37de10d23
OuisMusic <60331398+OuisMusic@users.noreply.github.com>
2024-07-09 17:49:08 +0200

    error catching

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,12 +1,56 @@
+import React, { Component, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+  error: any;
+  errorInfo: any;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false, error: null, errorInfo: null };
+  }
+
+  static getDerivedStateFromError(error: any): State {
+    return { hasError: true, error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    this.setState({
+      hasError: true,
+      error: error,
+      errorInfo: errorInfo
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong.</h2>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
+```
+```tsx
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ErrorBoundary from './ErrorBoundary';
 
 const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
 ReactDOM.render(
   <React.StrictMode>
-    <459App apiBaseUrl={apiBaseUrl} />
+    <ErrorBoundary>
+      <App apiBaseUrl={apiBaseUrl} />
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
